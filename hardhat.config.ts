953bbfc703bad36f5c1f4c177d34bec066e9d56b
CJ42 <cavallerajean@gmail.com>
2022-01-21 08:49:42 +0000

    chore: add Typescript types generation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
-import { task } from "hardhat/config";
+
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+import "@typechain/hardhat";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  typechain: {
+    outDir: "types",
+  },
 };
 
 // You need to export an object to set up your config
